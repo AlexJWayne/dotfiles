2e832236c70c7f64bfcea2ae87d52092439a465b
Alex Wayne <alex@beautifulpixel.com>
2025-11-08 20:49:59 -0800

    reminders icon

--- .config/sketchybar/items/spaces/app_icons.lua
+++ .config/sketchybar/items/spaces/app_icons.lua
@@ -14,6 +14,7 @@ local apps = {
     ["Numbers"] = "",
     ["Preview"] = "",
     ["PrusaSlicer"] = "󰹛",
+    ["Reminders"] = "",
     ["Safari"] = "󰀹",
     ["Spotify"] = "",
     ["Slack"] = "",
